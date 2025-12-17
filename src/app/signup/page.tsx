116c60fc28bdb37fc36f7489774b5dee484d893c
Mohanusquare <mohan@usquare.info>
2025-12-17 21:33:48 +0530

    missed commit

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -681,7 +681,7 @@ const SignupPage: React.FC = () => {
 
     // Extract parameters
     const userId = userData.reffralId || (userData as any).userId;
-    const referrerId = (userData as any).refferedBy || '';
+    const referrerId = (userData as any).refferedBy  || '';
     const email = userData.email;
 
     if (!userId || !email) {
